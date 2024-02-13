d8b78c5c4949f0eebf6c49bfc1f908bc0b926b77
cariq-02 <sneha.ranjitkumar@mycariq.com>
2024-02-13 15:07:00 +0530

    first commit

---
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.library;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.test_lib.CariqUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                CariqUtil.printLog("Library : ", "This is accessed from Library");
+            }
+        });
+    }
+}
---
+++ test-lib/src/main/java/com/example/test_lib/CariqUtil.java
@@ -0,0 +1,14 @@
+package com.example.test_lib;
+
+import android.util.Log;
+
+public class CariqUtil {
+
+    /*public static void main(String[] args) {
+
+    }*/
+
+    public static void printLog(String tag, String message) {
+        Log.d(tag, "printLog: " + message);
+    }
+}
